8292c4d486bd91ccd6d97a12d5fe1a0fe96e5f5d
Martin_Jordanov <martistj@gmail.com>
2019-12-27 16:08:09 +0200

    More experiments

--- src/trainer.py
+++ src/trainer.py
@@ -1,8 +1,14 @@
-import gym
-
+import gym, time
 env = gym.make('Seaquest-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(200):
+        env.render()
+        action = 8
+        observation, reward, done, info = env.step(action)
+        time.sleep(0.15)
+        print(observation[50])
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
 env.close()
