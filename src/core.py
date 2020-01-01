90cf88acb616763d2d9f485228eacad30beaf260
Martin_Jordanov <martistj@gmail.com>
2020-01-01 03:33:14 +0200

    New core methods  - act  - memorize  - load  - save
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- src/core.py
+++ src/core.py
@@ -1,10 +1,10 @@
+import random
 from collections import deque
 
+import numpy as np
 from keras.layers import Dense, Conv2D
 from keras.models import Sequential
 from keras.optimizers import Adam
-
-EPISODES = 1000
 
 
 class DQNAgent:
@@ -14,8 +14,7 @@ class DQNAgent:
         self.memory = deque(maxlen=2000)  # memory
         self.gamma = 0.95  # discount rate
         self.epsilon = 1.0  # exploration rate
-        # self.epsilon_min = 0.01
-        # self.epsilon_decay = 0.995
+        # TODO add decay rate
         self.learning_rate = 0.001
         self.model = self._build_model()
 
@@ -30,3 +29,17 @@ class DQNAgent:
                       optimizer=Adam(lr=self.learning_rate))
         return model
 
+    def memorize(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
