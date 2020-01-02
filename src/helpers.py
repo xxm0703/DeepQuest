4bb5a884518ab277ce1dfab258f8c8b350af62bc
Martin_Jordanov <martistj@gmail.com>
2020-01-02 16:42:29 +0200

    Plotting
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 __all__ = {
@@ -7,3 +8,27 @@ __all__ = {
 
 def rgb2gray(rgb):
     return np.dot(rgb, [0.2989, 0.5870, 0.1140])
+
+
+class LossPlotter:
+    STEP = 0
+
+    def __init__(self, x_axis='Time', y_axis='Loss', title='Real-time loss plot'):
+        self.x_axis = x_axis
+        self.y_axis = y_axis
+        self.title = title
+
+    def setup(self):
+        plt.title(self.title)
+        plt.ylabel(self.y_axis)
+        plt.xlabel(self.x_axis)
+
+    def plot_loss(self, value):
+        plt.plot(self.STEP, value, 'r+')
+        plt.pause(1)
+        plt.show()
+
+
+a = LossPlotter()
+a.plot_loss(13)
+a.plot_loss(10)
---
+++ src/stages/real-time-loss.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#
+#
+# i = 0
+# x = list()
+# y = list()
+#
+# while i < 100:
+#     temp_y = np.random.random()
+#     x.append(i)
+#     y.append(temp_y)
+#     plt.plot(i, i, '.-')
+#     i += 1
+#     plt.pause(0.0001)  # Note this correction
+# plt.show()
+
+
+x1 = np.linspace(0.0, 5.0)
+x2 = np.linspace(0.0, 2.0)
+y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+plt.subplot(2, 1, 1)
+plt.plot(x1, y1, 'o-')
+plt.title('A tale of 2 subplots')
+plt.ylabel('Damped oscillation')
+# plt.pause(0.0001)  # Note this correction
+
+plt.show()
