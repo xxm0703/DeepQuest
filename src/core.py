230c72c5790f94fa220e697a9501c1135895eacc
Martin_Jordanov <martistj@gmail.com>
2020-01-03 22:13:23 +0200

    New topology
    Signed-off-by: Martin_Jordanov <martistj@gmail.com>

--- src/core.py
+++ src/core.py
@@ -17,27 +17,27 @@ class DQNAgent:
         self.gamma = 0.8  # discount rate
         self.epsilon = 1  # exploration rate
         self.epsilon_decay = 5e-5
-        self.epsilon_min = 0.1
-        self.learning_rate = 0.001
+        self.epsilon_min = 0.02
+        self.learning_rate = 0.995
         self.model = self._build_model()
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential(name='Questor1.0')
-        model.add(Conv2D(kernel_size=5, filters=32, input_shape=self.state_size, activation='relu'))
+        model.add(Conv2D(kernel_size=5, filters=8, input_shape=self.state_size, activation='relu'))
         model.add(MaxPooling2D(pool_size=2))
 
-        model.add(Conv2D(kernel_size=3, filters=32, activation='relu'))
+        model.add(Conv2D(kernel_size=3, filters=16, activation='relu'))
         model.add(MaxPooling2D(pool_size=2))
 
-        model.add(Conv2D(kernel_size=3, filters=32, activation='relu'))
+        model.add(Conv2D(kernel_size=3, filters=8, activation='relu'))
         model.add(MaxPooling2D(pool_size=2))
 
         model.add(Dropout(0.2))
         model.add(Flatten())
 
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
+        model.add(Dense(256, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(64, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse',
                       optimizer=Adam(lr=self.learning_rate))
@@ -66,15 +66,25 @@ class DQNAgent:
             if not done:
                 next_state = encapsulator(next_state)
                 a = self.model.predict(next_state)
-                target = (2 * reward + self.gamma * np.amax(a[0]))
+                target = (reward + self.gamma * np.amax(a[0]))
 
             state = encapsulator(state)
             target_f = self.model.predict(state)
             target_f[0][action] = target
-            self.model.fit(state, target_f, verbose=False)  # verbose=0
+            history = self.model.fit(state, target_f, verbose=False)  # verbose=0
+
+            loss = history.history['loss'][0]
+
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
+
+            return loss
 
     def load(self, name):
         self.model.load_weights(name)
 
     def save(self, name):
         self.model.save_weights(name)
+
+    def plot(self, loss):
+        pass
